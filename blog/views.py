cbc543853205489ec72882aa803cb0a34a14f842
osas <osassylvester21@gmail.com>
2024-12-13 23:55:44 -0800

    Modifiled templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from .models import Post
 # Create your views here.
 
 def post_list(request, template_name='blog/post_list.html'):
-    return render(request, template_name, {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, template_name, {'posts': posts})
--- myapp/settings.py
+++ myapp/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-3ksj^8lya&0phf&@_34pk=#z@16c95u2*bk65u+gu=t%v=az2q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1:8000','local host','127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1:8000','local host','127.0.0.1','osas.pythonanywhere.com','www.osas.pythonanywhee.com']
 
 
 # Application definition
